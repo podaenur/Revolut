35f11413bd9afd0a37acded895ecb0c776f9e58c
Evgeniy Akhmerov <zzzz-w@yandex.ru>
2017-09-02 10:14:38 +0300

    Did initial project components

---
+++ Revolut/Revolut/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Revolut
+//
+//  Created by Evgeniy Akhmerov on 02/09/2017.
+//  Copyright © 2017 Evgeniy Akhmerov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Revolut/Revolut/AppDelegate.m
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.m
+//  Revolut
+//
+//  Created by Evgeniy Akhmerov on 02/09/2017.
+//  Copyright © 2017 Evgeniy Akhmerov. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+@end
